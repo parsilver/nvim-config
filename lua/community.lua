7daf3c61405057559425551ef957ec023ed5c3ee
parsilver <magician_canobi@hotmail.com>
2024-08-28 23:47:05 +0700

    Update status bar

--- lua/community.lua
+++ lua/community.lua
@@ -14,7 +14,6 @@ return {
     { import = "astrocommunity.docker.lazydocker" },
     { import = "astrocommunity.bars-and-lines.vim-illuminate" },
     { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
-    { import = "astrocommunity.bars-and-lines.lualine-nvim" },
 
     { import = "astrocommunity.git.diffview-nvim" },
     { import = "astrocommunity.git.gitgraph-nvim" },
@@ -22,6 +21,8 @@ return {
 
     { import = "astrocommunity.motion.flash-nvim" },
     { import = "astrocommunity.motion.nvim-surround" },
+
+    { import = "astrocommunity.indent.mini-indentscope" },
 
     -- { import = "astrocommunity.pack.php" },
     { import = "astrocommunity.pack.laravel" },
