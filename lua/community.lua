b5976710330e9b7bcb6e712b3c3a68bbbcd32069
parsilver <magician_canobi@hotmail.com>
2024-08-28 15:11:00 +0700

    Updatee

--- lua/community.lua
+++ lua/community.lua
@@ -11,6 +11,17 @@ return {
     { import = "astrocommunity.colorscheme.nightfox-nvim" },
     { import = "astrocommunity.recipes.telescope-nvchad-theme" },
     { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+    { import = "astrocommunity.docker.lazydocker" },
+    { import = "astrocommunity.bars-and-lines.vim-illuminate" },
+    { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+    { import = "astrocommunity.bars-and-lines.lualine-nvim" },
+
+    { import = "astrocommunity.git.diffview-nvim" },
+    { import = "astrocommunity.git.gitgraph-nvim" },
+    { import = "astrocommunity.git.octo-nvim" },
+
+    { import = "astrocommunity.motion.flash-nvim" },
+    { import = "astrocommunity.motion.nvim-surround" },
 
     -- { import = "astrocommunity.pack.php" },
     { import = "astrocommunity.pack.laravel" },
